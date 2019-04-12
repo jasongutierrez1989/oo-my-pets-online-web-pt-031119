59e360fa222e518da99138c4042ac4a7e094593b
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-12 01:33:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,13 +3,15 @@ class Owner
   @@count = 0
 
   attr_reader :species
-  attr_accessor :name
+  attr_accessor :name, :pets, :fishes, :dogs, :cats
 
   def initialize(species, name = nil)
     @species = species
     @name = name
+    @pets = {:fishes => [], :dogs => [], :cats => []}
     @@all << self
     @@count += 1
+    
   end
 
   def self.all
@@ -25,4 +27,7 @@ class Owner
     @@count = 0
   end
 
+  def say_species
+    @species
+  end
 end
