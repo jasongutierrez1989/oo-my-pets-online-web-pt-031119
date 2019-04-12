af18f734ec2a942f0937eecbe5cb2817c9a66294
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-12 01:25:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,17 +1,26 @@
 class Owner
-  ::all = []
-  ::count = 0
+  @@all = []
+  @@count = 0
+  
+  attr_reader :species
+  attr_accessor :name
 
+  def initialize(species, name)
+    @species = species
+    @name = name
+    @pets = Hash.new { |hash, key| hash[key] =  }
+  end
   def self.all
-    ::all
+    @@all
   end
 
   def self.count
-    ::count
+    @@count
   end
 
   def self.reset_all
-    ::all = []
-    ::count = 0
+    @@all = []
+    @@count = 0
   end
+
 end
