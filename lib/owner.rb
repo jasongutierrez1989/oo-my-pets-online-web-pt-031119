d0edbd96d5b289afe612db8749f96a2110f50212
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-12 02:01:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,6 @@ class Owner
   end
 
   def list_pets
-    return "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s).
-"
+    return "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
   end
 end
