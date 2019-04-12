6713515ff1d16a4ee592a036d4d706dad47ab49e
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-12 01:18:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,17 @@
 class Owner
-  # code goes here
+  ::all = []
+  ::count = 0
+
+  def self.all
+    ::all
+  end
+
+  def self.count
+    ::count
+  end
+
+  def self.reset_all
+    ::all = []
+    ::count = 0
+  end
 end
